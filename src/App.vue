7459d30e4e21c669336dfe590de16b082d884f74
Chainlayer | Peter <peter@chainlayer.io>
2019-07-01 13:33:29 +0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CosmosLedger msg="Minimal Cosmos+Ledger delegation example"/>
+  </div>
+</template>
+
+<script>
+import CosmosLedger from './components/CosmosLedger.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CosmosLedger
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CosmosLedger.vue
@@ -0,0 +1,201 @@
+<template>
+    <div class="cosmosLedger">
+        <button v-on:click="tryConnect">Connect</button><br>
+        <label>Connected: </label><span>{{connected}}</span><br>
+        <label>Address: </label><span>{{bech32}}</span><br>
+        <label>Public Key: </label><span>{{pk}}</span><br>
+        <label>Path: </label><span>{{path}}</span><br>
+        <label>Available Balance: </label><span>{{balance_available}}</span><br>
+        <label>Delegated Balance: </label><span>{{balance_delegated}}</span><br>
+        <label>Total Balance: </label><span>{{balance_total}}</span><br>
+        <label>Sequence: </label><span>{{sequence}}</span><br>
+        <label>accountNumber: </label><span>{{accInfo.accountNumber}}</span><br>
+        <label v-if="this.readytodelegate">Delegation amount: </label><input v-model.number="delegation" type="number" v-if="this.readytodelegate"><br>
+        <button v-on:click="delegate" v-if="this.readytodelegate">Delegate</button>
+        <hr>
+        <ul id="console-status">
+            <li v-for="item in consoleStatus" v-bind:key="item.index">
+                {{ item.msg }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    var { IrisDelegateTool } = require("cosmos-sdk-delegation-lib");
+    import TransportU2F from '@ledgerhq/hw-transport-u2f';
+    import Big from 'big.js';
+
+    const transport = new TransportU2F();
+    const cdt = new IrisDelegateTool(transport);
+    cdt.setNodeURL('https://192.168.2.101');
+
+    export default {
+        name: 'CosmosLedger',
+        props: {
+            restUrl: String,
+        },
+        data() {
+            return {
+                consoleLog: [],
+                myAddr: '',
+                bech32: '',
+                pk: '',
+                path: '',
+                accInfo: '',
+                sequence: '',
+                delegation: '',
+                reply: '',
+                balance_available: '',
+                balance_delegated: '',
+                balance_total: '',
+                connected: 'false',
+                readytodelegate: false
+            }
+        },
+        computed: {
+            consoleStatus() {
+                return this.consoleLog;
+            },
+        },
+        methods: {
+            log: function (list, msg) {
+                list.push({
+                    index: list.length,
+                    msg: msg
+                })
+            },
+            tryConnect: async function () {
+                this.consoleLog = [];
+                this.myAddr = null;
+                this.readytodelegate = false;
+
+                this.log(this.consoleLog, "Trying to connect...");
+
+                try {
+                    await cdt.connect();
+                } catch(e) {
+                    // TODO: Handle error if not logged in
+                    this.log(this.consoleLog, e);
+                    this.connected = 'false';
+                    return;
+                }
+                if (!cdt.connected) {
+                    this.connected = 'false';
+                    this.log(this.consoleLog, cdt.lastError);
+                    return;
+                }
+                this.connected = 'true';
+                this.log(this.consoleLog, "Connected!");
+
+                try {
+                    this.myAddr = await cdt.retrieveAddress(0, 0);
+                } catch(e) {
+                    // TODO: Handle error if not logged in
+                    this.log(this.consoleLog, e);
+                    return
+                }
+                this.bech32 = this.myAddr.bech32;
+                this.pk = this.myAddr.pk;
+                this.path = this.myAddr.path;
+                this.log(this.consoleLog, this.myAddr);
+                this.log(this.consoleLog, `Address  : ${this.myAddr.bech32}`);
+                this.log(this.consoleLog, `PublicKey: ${this.myAddr.pk}`);
+                this.getBalance();
+            },
+
+            getBalance: async function () {
+                if (!cdt.connected) {
+                    this.log(this.consoleLog, "Try connecting first..");
+                    return;
+                }
+
+                if (this.myAddr == null) {
+                    this.log(this.consoleLog, "Retrieve the device address first");
+                    return;
+                }
+
+                this.log(this.consoleLog, `Query ${this.myAddr.bech32}`);
+
+                this.accInfo = await cdt.getAccountInfo(this.myAddr);
+                if (this.accInfo.error) {
+                    this.log(this.consoleLog, this.accInfo.error);
+                    return
+                } else {
+                    this.log(this.consoleLog, this.accInfo.error);
+                    this.log(this.consoleLog, this.accInfo);
+                    this.sequence = this.accInfo.sequence;
+                    this.balance_available = Big(this.accInfo.balanceirisatta / 1000000000000000000);
+                    this.log(this.consoleLog, this.accInfo);
+                }
+
+                this.reply = await cdt.retrieveBalances([this.myAddr]);
+                if (this.accInfo.error) {
+                    this.log(this.consoleLog, this.reply.error);
+                    return
+                } else {
+                    this.log(this.consoleLog, this.reply);
+                    this.balance_delegated = Big(this.reply[0].delegationsTotaluAtoms);
+                    this.balance_total = this.balance_delegated.add(this.balance_available);
+                    this.log(this.consoleLog, this.reply[0].delegationsTotaluAtoms);
+                }
+                this.readytodelegate = true;
+            },
+            delegate: async function () {
+                if (!cdt.connected) {
+                    this.log(this.consoleLog, "Try connecting first..");
+                    return;
+                }
+                if (this.myAddr === null) {
+                    this.log(this.consoleLog, "Retrieve the device address first");
+                    return;
+                }
+                const txContext = {
+                    chainId: 'irishub',
+                    path: this.myAddr.path,
+                    bech32: this.myAddr.bech32,
+                    pk: this.myAddr.pk,
+                };
+
+                const dummyTx = await cdt.txCreateDelegate(
+                    txContext,
+                    'iva1kgddca7qj96z0qcxr2c45z73cfl0c75pmw0meu',
+                    this.delegation,   // TODO change this
+                    'Delegation to ChainLayer.io',
+                );
+
+                this.log(this.consoleLog, "Waiting for device to sign");
+
+                const signedTx = await cdt.sign(dummyTx, txContext);
+
+                this.log(this.consoleLog, "Broadcasting signed tx");
+                const response = await  cdt.txSubmit(signedTx);
+
+                this.log(this.consoleLog, response);
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    button {
+        padding: 5px;
+        font-weight: bold;
+        font-size: medium;
+    }
+
+    ul {
+        padding: 10px;
+        text-align: left;
+        alignment: left;
+        list-style-type: none;
+        background: black;
+        font-weight: bold;
+        color: greenyellow;
+    }
+</style>
